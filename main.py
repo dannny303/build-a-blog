7ee05dd8c643188460258ae3a499be22293132dd
Danny Marez <750danny@gmail.com>
2018-07-12 11:15:49 -0600

    database hookup

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogblogblog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buildablog:myfirstblog@localhost:8889/buildablog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'Lm4YWvQ6jYMT'
@@ -25,15 +25,13 @@ posts = [
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    entry = db.Column(db.String(500))
+    title = db.Column(db.String(120), unique=True)
+    content = db.Column(db.Text(500))
    
-
-    def __init__(self, name):
-        self.name = name
-        self.blog = blog
-
-
+def __init__(self, title):
+    self.title = title
+    
+    
 
 @app.route('/')
 @app.route('/blog')
